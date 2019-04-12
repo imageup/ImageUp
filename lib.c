90e809cc40ebc814ad5b69ad9b8603cdabfd4224
WCLL_PLT <deertrodis@hotmail.com>
2019-04-12 19:22:09 +0000

    add library code

---
+++ lib.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+double* transpose(double* mat, int r, int c) {
+    double *p1 =(double *) malloc(r * c * sizeof(double));
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            p1[j * r + i] = mat[i * c + j];
+        }
+    }
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            mat[i * c + j] = p1[i * c + j];
+        }
+    }
+    free(p1);
+    return mat;
+}
+
+double* scale(double* mat, int r, int c,  double ratio) {
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            mat[i * c + j] *= ratio;
+        }
+    }
+    return mat;
+}
+
+double* rorate(double* mat, int r, int c, bool dir) {
+    mat = transpose(mat, r, c);
+    double *p1 = (double *)malloc(r * c * sizeof(double));
+    if (dir == true) {//left
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                p1[(r - 1 - i) * c + j] = mat[i * c + j];
+            }
+        }
+    }
+    else {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                p1[i * c + (c - 1 - j)] = mat[i * c + j];
+            }
+        }
+    }
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            mat[i * c + j] = p1[i * c + j];
+        }
+    }
+    free(p1);
+    return mat;
+}
