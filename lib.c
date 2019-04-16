020cf2ec3a5338893a25b9097e47ff885d050b99
WCLL_PLT <deertrodis@hotmail.com>
2019-04-16 00:01:44 +0000

    fix transpose, make it work

--- lib.c
+++ lib.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#define MATRIX_SIZE 200
 double* transpose_c(double* mat, int r, int c) {
-    double *p1 =(double *) malloc(r * c * sizeof(double));
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            p1[j * r + i] = mat[i * c + j];
+    double *p1 =(double *) malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(double));
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            p1[j * MATRIX_SIZE + i] = mat[i * MATRIX_SIZE + j];
         }
     }
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            mat[i * c + j] = p1[i * c + j];
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            mat[i * MATRIX_SIZE + j] = p1[i * MATRIX_SIZE + j];
         }
     }
     free(p1);
@@ -27,26 +27,26 @@ void scale_c(double* mat, int r, int c,  double ratio) {
 
 }
 
-double* rorate(double* mat, int r, int c, bool dir) {
+double* rotate_c(double* mat, int r, int c, bool dir) {
     mat = transpose_c(mat, r, c);
-    double *p1 = (double *)malloc(r * c * sizeof(double));
+    double *p1 = (double *)malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(double));
     if (dir == true) {//left
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                p1[(r - 1 - i) * c + j] = mat[i * c + j];
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            for (int j = 0; j < MATRIX_SIZE; j++) {
+                p1[(MATRIX_SIZE - 1 - i) * MATRIX_SIZE + j] = mat[i * MATRIX_SIZE + j];
             }
         }
     }
     else {
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                p1[i * c + (c - 1 - j)] = mat[i * c + j];
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            for (int j = 0; j < MATRIX_SIZE; j++) {
+                p1[i * MATRIX_SIZE + (MATRIX_SIZE - 1 - j)] = mat[i * MATRIX_SIZE + j];
             }
         }
     }
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            mat[i * c + j] = p1[i * c + j];
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            mat[i * MATRIX_SIZE + j] = p1[i * MATRIX_SIZE + j];
         }
     }
     free(p1);
